80fbbea42852334ea38db293c83341465a7bb398
engelsgermosen <mrengels2648@gmail.com>
2025-04-14 16:17:12 -0400

    e

--- src/components/CryptoPage.jsx
+++ src/components/CryptoPage.jsx
@@ -1,13 +1,16 @@
-import LoadingCircle from './LoadingCircle';
-import { useParams } from 'react-router';
-import '../styles/CryptoPages.css';
-import usePetition from '../hooks/usePetition';
+import LoadingCircle from "./LoadingCircle";
+import { useParams } from "react-router";
+import "../styles/CryptoPages.css";
+import usePetition from "../hooks/usePetition";
 
 const CryptoPage = () => {
-
   const { id } = useParams();
-  const data = usePetition(`https://api.coincap.io/v2/assets/${id}`);
-  const history = usePetition(`https://api.coincap.io/v2/assets/${id}/history?interval=d1`);
+  const data = usePetition(
+    `https://rest.coincap.io/v3/assets/${id}?apiKey=d7a8c72e3b339b06959c5bf63dba53d4ccba0adaac70546554e2dcfa5d02f203`
+  );
+  const history = usePetition(
+    `https://rest.coincap.io/v3/assets/${id}/history?interval=d1&apiKey=d7a8c72e3b339b06959c5bf63dba53d4ccba0adaac70546554e2dcfa5d02f203`
+  );
 
   if (!data || !history) return <LoadingCircle />;
 
@@ -15,54 +18,61 @@ const CryptoPage = () => {
     <div className="container">
       <aside className="aside">
         <div>
-          <p className='label-datos'>
-            Rank: <span className='infro-datos'>{data.rank}</span>
+          <p className="label-datos">
+            Rank: <span className="infro-datos">{data.rank}</span>
           </p>
-          <p className='label-datos'>
-            Name: <span className='infro-datos'>{data.name}</span>
+          <p className="label-datos">
+            Name: <span className="infro-datos">{data.name}</span>
           </p>
-          <p className='label-datos'>
-            Symbol: <span className='infro-datos'>{data.symbol}</span>
+          <p className="label-datos">
+            Symbol: <span className="infro-datos">{data.symbol}</span>
           </p>
-          <p className='label-datos'>
-            Supply: <span className='infro-datos'>{parseFloat(data.supply).toFixed(3)}</span>
+          <p className="label-datos">
+            Supply:{" "}
+            <span className="infro-datos">
+              {parseFloat(data.supply).toFixed(3)}
+            </span>
           </p>
-          <p className='label-datos'>
-            MaxSupply: <span className='infro-datos'>{isNaN(parseFloat(data.maxSupply).toFixed(3)) ? "Unknown" : parseFloat(data.maxSupply).toFixed(3)}</span>
+          <p className="label-datos">
+            MaxSupply:{" "}
+            <span className="infro-datos">
+              {isNaN(parseFloat(data.maxSupply).toFixed(3))
+                ? "Unknown"
+                : parseFloat(data.maxSupply).toFixed(3)}
+            </span>
           </p>
-          <p className='label-datos'>
-            MarketCapUsd: <span className='infro-datos'>{parseFloat(data.marketCapUsd).toFixed(3)}</span>
+          <p className="label-datos">
+            MarketCapUsd:{" "}
+            <span className="infro-datos">
+              {parseFloat(data.marketCapUsd).toFixed(3)}
+            </span>
           </p>
         </div>
       </aside>
       <main>
         <div className="table-container">
-          {
-            history && (
-              <table className="table">
-                <thead className="thead">
-                  <tr>
-                    <th className="th">Fecha</th>
-                    <th className="th">Precio</th>
+          {history && (
+            <table className="table">
+              <thead className="thead">
+                <tr>
+                  <th className="th">Fecha</th>
+                  <th className="th">Precio</th>
+                </tr>
+              </thead>
+              <tbody className="tbody">
+                {history.map(({ priceUsd, date, time }) => (
+                  <tr key={time} className="tr">
+                    <td className="td">{new Date(date).toDateString()}</td>
+                    <td className="td">{parseFloat(priceUsd).toFixed(3)}</td>
                   </tr>
-                </thead>
-                <tbody className="tbody">
-                  {
-                    history.map(({ priceUsd, date, time }) => (
-                      <tr key={time} className="tr">
-                        <td className="td">{new Date(date).toDateString()}</td>
-                        <td className="td">{parseFloat(priceUsd).toFixed(3)}</td>
-                      </tr>
-                    ))
-                  }
-                </tbody>
-              </table>
-            )
-          }
+                ))}
+              </tbody>
+            </table>
+          )}
         </div>
       </main>
     </div>
   );
-}
+};
 
 export default CryptoPage;
