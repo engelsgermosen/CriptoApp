3e8541621e7c7b298b20798ff913ed5d31b78f29
engelsgermosen <mrengels2648@gmail.com>
2025-04-14 16:09:05 -0400

    e

--- src/components/Cuadricula.jsx
+++ src/components/Cuadricula.jsx
@@ -1,23 +1,25 @@
-import { useEffect, useState } from 'react'
-import '../styles/Cuadricula.css'
-import LoadingCircle from './LoadingCircle'
-import Cryptos from './Cryptos'
+import { useEffect, useState } from "react";
+import "../styles/Cuadricula.css";
+import LoadingCircle from "./LoadingCircle";
+import Cryptos from "./Cryptos";
 
 function Cuadricula() {
-  const [cryptos, setCryptos] = useState()
+  const [cryptos, setCryptos] = useState();
 
   useEffect(() => {
-    fetch('https://api.coincap.io/v2/assets')
-      .then(res => res.json())
-      .then(data => setCryptos(data.data))
-      .catch(error => console.error(error))
-  }, [])
+    fetch(
+      "https://rest.coincap.io/v3/assets?apiKey=d7a8c72e3b339b06959c5bf63dba53d4ccba0adaac70546554e2dcfa5d02f203"
+    )
+      .then((res) => res.json())
+      .then((data) => setCryptos(data.data))
+      .catch((error) => console.error(error));
+  }, []);
 
-  if (!cryptos) return <LoadingCircle />
+  if (!cryptos) return <LoadingCircle />;
 
   return (
     <div className="crypto-container">
-      <div className='crypto-content'>
+      <div className="crypto-content">
         <h1 className="crypto-page-title">Criptomonedas</h1>
         <div className="crypto-grid-container">
           {cryptos.map(({ id, changePercent24Hr, symbol, name, priceUsd }) => (
@@ -34,8 +36,6 @@ function Cuadricula() {
       </div>
     </div>
   );
-
-
 }
 
-export default Cuadricula
+export default Cuadricula;
